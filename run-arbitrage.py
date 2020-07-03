18cc91009f1c0a441d00e98f8ae2cb62ddd849b4
Julien Klepatch <julien@julienklepatch.com>
2020-07-03 20:14:41 +0800

    fix

--- run-arbitrage.py
+++ run-arbitrage.py
@@ -1,12 +1,14 @@
 from web3 import Web3
 import json
 
-#import build/contracts/Arbitrage.json
-
-
+# to fill
 INFURA_URL = '';
+SRC_TOKEN_ADDRESS = ''
+ARBITRAGE_ADDRESS = ''
+OWNER_ADDRESS = ''
+OWNER_PRIVATE_KEY = ''
+
 w3 = Web3(Web3.HTTPProvider(INFURA_URL)
-w3.eth.defaultAccount = ''
 
 arbitrage_abi = json.loads('[
     {
@@ -90,31 +92,252 @@ arbitrage_abi = json.loads('[
 )
 
 # to add
-#erc20_abi = json.loads()
+erc20_abi = json.loads('
+[
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "balance",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    }
+]')
 
 gas_price = Web3.eth.generateGasPrice()
-src_token = web3.eth.contract(abi=erc20_abi, address=src_token_address) 
-arbitrage = web3.eth.contract(abi=arbitrage_abi, address=arbitrage_address) 
+src_token = web3.eth.contract(abi=erc20_abi, address=SRC_TOKEN_ADDRESS) 
+arbitrage = web3.eth.contract(abi=arbitrage_abi, address=ARBITRAGE_ADDRESS) 
 
 tx1 = src_token.functions
     .approve(arbitrage_address, 1000) 
     .buildTransaction({
         'chainId': 1, 
         'gasPrice': gas_price, 
-        'nonce': web3.eth.getTransactionCount(user_address), 
+        'nonce': web3.eth.getTransactionCount(OWNER_ADDRESS), 
     })
-signed_tx1 = web3.eth.account.signTransaction(tx1, private_key)
+signed_tx1 = web3.eth.account.signTransaction(tx1, OWNER_PRIVATE_KEY)
 tx1_hash = web3.eth.sendRawTransaction(signed_tx1.rawTransaction)
 print(tx1_hash)
 
-tx2 = txarbitrage.functions
+tx2 = arbitrage.functions
     .runTokenKyberUniswap(1000, src_address, src_address)
     .buildTransaction({
         'chainId': 1, 
         'gasPrice': gas_price, 
-        'nonce': web3.eth.getTransactionCount(user_address), 
-        'value': 0
+        'nonce': web3.eth.getTransactionCount(OWNER_ADDRESS), 
     })
-signed_tx2 = web3.eth.account.signTransaction(tx2, private_key)
+signed_tx2 = web3.eth.account.signTransaction(tx2, OWNER_PRIVATE_KEY)
 tx2_hash = web3.eth.sendRawTransaction(signed_tx2.rawTransaction)
 print(tx2_hash)
